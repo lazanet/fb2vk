960585cfb7d7915aad8960e2f60b338ab4a394d3
lazanet <lazanet96@gmail.com>
2019-09-07 11:00:30 +0200

    Log file location

--- wrapper.py
+++ wrapper.py
@@ -2,6 +2,6 @@
 # -*- coding: utf-8 -*-
 import time, os
 while True:
-	os.system("python3 ./main.py > ./log.txt 2>&1")
+	os.system("python3 ./main.py > ./logs/log.txt 2>&1")
 	print("Done one turn!")
 	time.sleep(600)
